46bbd7d08994a688c9415ae9530e4168558eba7b
crollo94 <kelly.s826508@stu.sanjac.edu>
2025-03-28 18:46:28 +0000

    implemented basic banking system

--- Banking-System/main.cpp
+++ Banking-System/main.cpp
@@ -6,42 +6,50 @@
 #include<iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int option;
     double avaivable = 0.0;
-    double total;
-
-    while(true) {
+    double total= 0.0;
+    bool running = true;
+    
         cout << " ======Banking System===== " << endl;
         cout << " 1.Deposit money" << endl;
         cout << " 2.Withdraw money" << endl;
         cout << " 3.Check Balance" << endl;
         cout << " 4.Exit" << endl;
         cout << " ========================= " << endl;
+    while(running) 
+    {
         cout << " pick one please" << endl;
         cin >> option;
     
-        switch(option) {
+        switch(option) 
+        {
+
             case 1:
-                cout << "enter the total to deposit";
+                cout << "enter the total to deposit" << endl;
                 cin >> total;
-                if (total > 0) {
-                    avaivable += total;
-                    cout << " money deposited new total is: " << avaivable << endl;
+                if (total > 0) 
+                {
+                    avaivable += total; 
+                    cout << " money deposit1ed new total is: $" << avaivable << endl;
                 
-                } else {
+                } else 
+                {
                     cout << "this amount does not work" << endl;
                 } break;
 
             case 2:
             cout << "please enter the amount to withdraw";
             cin >> total;
-            if (total > 0 & total <= avaivable){
+            if (total > 0 & total <= avaivable)
+            {
                 avaivable -= total;
                 cout << " new total is: " << avaivable << endl;
-            } else if(total > avaivable){
-                cout << "no funds left" << endl;
-            } else {
+            } 
+             else 
+             {
                 cout << " not enough money!" << endl;
             }
             break;
@@ -52,9 +60,16 @@ int main(){
             
             case 4: 
             cout << "exiting, thank you!" << endl;
-            return 0;
+            running = false;
+            break;
+
+            default:
+            cout << "invaild option" << endl;
+            break;
+
+        
         }
     
-        }    
+    }    
     return 0;
 }    
